52f05b8e7557253c83c9075ba9a8dcd5b40f6689
Nitin Shukla <nitin.shukla12004@gmail.com>
2026-04-20 10:58:27 +0000

    added basic code

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-import flask from Flask
+from flask import Flask 
+
+app = Flask(__name__)
+
+users = {"nitin" : "shukla"}
+
+@app.get("/")
+def hello():
+    return "Hello world"
+
+@app.post("/set-name/<first>/<last>")
+def setName(first, last):
+    users[first] = last
+    return "lastName entered successfully"
+@app.get("/get-name/<first>")
+def getName(first):
+    if first not in users:
+        return "Enter a valid firstName"
+    else:
+        return users[first]
+
